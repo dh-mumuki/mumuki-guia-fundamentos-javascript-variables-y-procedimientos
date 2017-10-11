aad7bd5211aa5ddac65b84843eacd399b2cf7272
franco@mumuki.org <franco@mumuki.org>
2017-10-11 11:09:15 -0400

    Mumuki Export on 2017-10-11 11:09:15 -0400

--- 00005_¿Y los procedimientos?/test..js
+++ 00005_¿Y los procedimientos?/test..js
@@ -1,7 +1,7 @@
 describe("versos", function() {
   it("deberia imprimir los versos", function() {
     versosMartinFierro()
-    assert.equal(console.toString(), "Aquí me pongo a cantar\nAl compás de la vigüela;\nQue el hombre que lo desvela\nUna pena extraordinaria\n")
+    assert.equal(fakeConsole.toString(), "Aquí me pongo a cantar\nAl compás de la vigüela;\nQue el hombre que lo desvela\nUna pena extraordinaria\n")
   });
   it("no deberia retornar nada", function() {
     assert(versosMartinFierro() === undefined);
